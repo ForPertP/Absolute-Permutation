265e5f9e85118acf6314e085628742bbdf2740b8
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-11-16 05:36:27 +0900

    Update Absolute-Permutation.cpp

--- Absolute-Permutation.cpp
+++ Absolute-Permutation.cpp
@@ -66,3 +66,40 @@ vector<int> absolutePermutation(int n, int k)
         return result;
     }
 }
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int k = stoi(first_multiple_input[1]);
+
+        vector<int> result = absolutePermutation(n, k);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            fout << result[i];
+
+            if (i != result.size() - 1) {
+                fout << " ";
+            }
+        }
+
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}

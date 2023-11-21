51fb53bd5b450be042b8e017711bd06becb41298
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-11-22 00:16:29 +0900

    Update Absolute-Permutation.cs

--- Absolute-Permutation.cs
+++ Absolute-Permutation.cs
@@ -15,3 +15,30 @@ using System;
 class Result
 {
 }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int k = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> result = Result.absolutePermutation(n, k);
+
+            textWriter.WriteLine(String.Join(" ", result));
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}

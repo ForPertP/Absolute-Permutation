73146416d80d45d7dece5aeccce10eefe1359b9d
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-12-01 06:00:31 +0900

    Update Absolute-Permutation.cpp

--- Absolute-Permutation.cpp
+++ Absolute-Permutation.cpp
@@ -15,9 +15,7 @@ vector<string> split(const string &);
  *  2. INTEGER k
  */
 
-
-vector<int> absolutePermutation(int n, int k)
-{
+vector<int> absolutePermutation(int n, int k) {
     vector<int> result;
 
     if (k == 0)
@@ -31,9 +29,9 @@ vector<int> absolutePermutation(int n, int k)
     {
         for (int i = 1, j = 1; i <= n; ++i)
         {
-            result.emplace_back((j++ <= k) ? i + k : i - k);
+            result.emplace_back((j <= k) ? i + k : i - k);
 
-            if (j > k * 2)
+            if (++j > k * 2)
             {
                 j = 1;
             }
@@ -45,57 +43,6 @@ vector<int> absolutePermutation(int n, int k)
     }
 
     return result;
-}
-
-vector<int> absolutePermutation2(int n, int k) {
-    vector<int> result;
-
-    if (k != 0)
-    {
-        if (n % 2 == 1)
-        {
-            return { -1 };
-        }
-
-        if (n % 2 == 0 && n % (k*2) == 0 )
-        {
-            int j = 1;
-
-            for (int i = 1; i <= n; ++i)
-            {
-                if (j <= k)
-                {
-                    result.emplace_back(i+k);
-                }
-                else
-                {
-                    result.emplace_back(i-k);
-                }
-
-                j++;
-                
-                if (j > k*2)
-                {
-                    j = 1;
-                }
-            }
-
-            return result;
-        }
-        else
-        {
-            return {-1};
-        }
-    }
-    else
-    {
-        for (int i = 1; i <= n; ++i)
-        {
-            result.emplace_back(i);
-        }
-        
-        return result;
-    }
 }
 
 int main()
